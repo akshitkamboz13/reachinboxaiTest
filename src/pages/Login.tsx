2d5aa470fcdaaa3f484bc18f3bdd3448244402cd
Akshit Kamboj <akshitkamboz13@gmail.com>
2024-08-27 12:04:21 +0530

    fix deploy

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,10 +5,10 @@ import logo from '../assets/logo.png'
 const Login = () => {
 
     useEffect(()=>{
-        localStorage.removeItem("reachinbox-auth");
-        localStorage.removeItem("reachinbox-auth-firstname");
-        localStorage.removeItem("reachinbox-auth-lastname");
-        localStorage.removeItem("reachinbox-auth-email");
+        // localStorage.removeItem("reachinbox-auth");
+        // localStorage.removeItem("reachinbox-auth-firstname");
+        // localStorage.removeItem("reachinbox-auth-lastname");
+        // localStorage.removeItem("reachinbox-auth-email");
     },[])
   return (
     <div className='bg-black max-w-[1440px] h-screen m-auto text-white  flex flex-col justify-between'>
@@ -21,7 +21,7 @@ const Login = () => {
                     <p  className=' text-xl mb-6'>Create new account</p>
                     <div className='rounded border border-gray-500 h-12 flex justify-center items-center gap-2.5  mb-12'>
                         <img src="https://static.vecteezy.com/system/resources/previews/013/760/951/non_2x/colourful-google-logo-in-dark-background-free-vector.jpg" alt=""  className='bg-black rounded-full h-5 mt-0.5'/>
-                        <Link  to="https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://si4k.me/reachinboxaiTest/desktop" className='text-sm text-gray-400 cursor-pointer'> Sign Up with Google </Link>
+                        <Link  to="https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://reachinbox-frontend.netlify.app/desktop" className='text-sm text-gray-400 cursor-pointer'> Sign Up with Google </Link>
                         {/* <Link  to="https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=http://localhost:3000/desktop" className='text-sm text-gray-400 cursor-pointer'> Sign Up with Google </Link> */}
                     </div>
                     <p className='w-[195px] h-12 bg-[#4B63DD] rounded m-auto text-center pt-2.5 mb-6 cursor-pointer'>Create an account</p>
